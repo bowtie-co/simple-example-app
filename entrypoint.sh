90f608998757bdb1081e3e2cff20e72678b21bac
Charlie McClung <charlie@bowtie.co>
2019-11-14 07:42:14 -0700

    Also show full env var dump for example

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,8 @@ env
 
 APP_ENV=${APP_ENV:-staging}
 APP_TAG=${APP_BUILD_TAG:-latest}
+APP_FULL_ENV=$(env)
+APP_FULL_ENV_PRE="<pre>${APP_FULL_ENV}</pre>"
 
 NGINX_INSTALL_PATH=/etc/nginx
 
@@ -14,5 +16,7 @@ for html in $SRV_DIR/*.html; do
   sed -i "s/PLACEHOLDER_APP_ENV/${APP_ENV}/g" "${html}"
   sed -i "s/PLACEHOLDER_APP_TAG/${APP_TAG}/g" "${html}"
 done
+
+echo "${APP_FULL_ENV_PRE}" > $SRV_DIR/env.html
 
 exec "$@"
